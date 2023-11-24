146e22d7ad43ebc7588f4e2c93c526ed0b10be84
Oscar Saul <oscarsaul.mmv@gmail.com>
2023-11-24 17:50:04 -0500

    Update script.py

--- script.py
+++ script.py
@@ -2,19 +2,24 @@ import requests
 from datetime import datetime, timedelta
 import re
 
+def is_server_running():
+    try:
+        response = requests.get('http://localhost:3000/status')
+        return response.status_code == 200
+    except requests.exceptions.RequestException:
+        return False
+
 def is_user_active(username):
     response = requests.get(f'https://api.github.com/users/{username}/events/public')
     events = response.json()
 
     github_active = False
-    vscode_active = False
+    vscode_active = is_server_running()
 
     for event in events:
         event_date = datetime.strptime(event['created_at'], '%Y-%m-%dT%H:%M:%SZ')
         if event_date > datetime.now() - timedelta(days=1):
             github_active = True
-            if 'VSCode' in event['repo']['name']:
-                vscode_active = True
 
     return github_active, vscode_active
 
@@ -27,7 +32,7 @@ def update_readme(username, github_active, vscode_active):
     else:
         content = re.sub(r'https://img.shields.io/static/v1\?label=GitHub&message=\w+&color=\w+', 'https://img.shields.io/static/v1?label=GitHub&message=Inactive&color=lightgrey', content)
     if vscode_active:
-        content = re.sub(r'https://img.shields.io/static/v1\?label=VSCode&message=\w+&color=\w+', 'https://img.shields.io/static/v1?label=VSCode&message=Active&color=brightgreen', content)
+        content = re.sub(r'https://img.shields.io/static/v1\?label=VSCode&message=\w+&color=\w+', 'https://img.shields.io/static/v1?label=VSCode&message=Active&color=skyblue', content)
     else:
         content = re.sub(r'https://img.shields.io/static/v1\?label=VSCode&message=\w+&color=\w+', 'https://img.shields.io/static/v1?label=VSCode&message=Inactive&color=lightgrey', content)
 
