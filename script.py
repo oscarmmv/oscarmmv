e788a21e41e571ccbe86836742ec91ede4f05434
Oscar Saul <oscarsaul.mmv@gmail.com>
2023-11-24 10:49:04 -0500

    Update script.py

--- script.py
+++ script.py
@@ -6,23 +6,32 @@ def is_user_active(username):
     response = requests.get(f'https://api.github.com/users/{username}/events/public')
     events = response.json()
 
+    github_active = False
+    vscode_active = False
+
     for event in events:
         event_date = datetime.strptime(event['created_at'], '%Y-%m-%dT%H:%M:%SZ')
         if event_date > datetime.now() - timedelta(days=1):
-            return True
+            github_active = True
+            if 'VSCode' in event['repo']['name']:
+                vscode_active = True
 
-    return False
+    return github_active, vscode_active
 
-def update_readme(username, active):
+def update_readme(username, github_active, vscode_active):
     with open('README.md', 'r+') as file:
         content = file.read()
         file.seek(0)
-        if active:
-            file.write(re.sub(r'https://img.shields.io/static/v1\?label=Activity&message=\w+&color=\w+', 'https://img.shields.io/static/v1?label=Activity&message=Active&color=brightgreen', content))
+        if github_active:
+            file.write(re.sub(r'https://img.shields.io/static/v1\?label=GitHub&message=\w+&color=\w+', 'https://img.shields.io/static/v1?label=GitHub&message=Active&color=brightgreen', content))
         else:
-            file.write(re.sub(r'https://img.shields.io/static/v1\?label=Activity&message=\w+&color=\w+', 'https://img.shields.io/static/v1?label=Activity&message=Inactive&color=lightgrey', content))
+            file.write(re.sub(r'https://img.shields.io/static/v1\?label=GitHub&message=\w+&color=\w+', 'https://img.shields.io/static/v1?label=GitHub&message=Inactive&color=lightgrey', content))
+        if vscode_active:
+            file.write(re.sub(r'https://img.shields.io/static/v1\?label=VSCode&message=\w+&color=\w+', 'https://img.shields.io/static/v1?label=VSCode&message=Active&color=brightgreen', content))
+        else:
+            file.write(re.sub(r'https://img.shields.io/static/v1\?label=VSCode&message=\w+&color=\w+', 'https://img.shields.io/static/v1?label=VSCode&message=Inactive&color=lightgrey', content))
         file.truncate()
 
 username = 'oscarmmv'
-active = is_user_active(username)
-update_readme(username, active)
+github_active, vscode_active = is_user_active(username)
+update_readme(username, github_active, vscode_active)
